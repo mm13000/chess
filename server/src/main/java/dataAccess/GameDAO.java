49d198465f3316fb9602a39b7430cab84e42bddd
Michael Murdock <mm13000@byu.edu>
2024-02-28 20:53:26 -0700

    Implement GameDAOMemory

--- server/src/main/java/dataAccess/GameDAO.java
+++ server/src/main/java/dataAccess/GameDAO.java
@@ -1,5 +1,13 @@
 package dataAccess;
 
+import model.GameData;
+
+import java.util.List;
+
 public interface GameDAO {
-    void clearGames();
+    void clearGames() throws DataAccessException;
+    void createGame(String gameName) throws DataAccessException;
+    List<GameData> listGames() throws DataAccessException;
+    GameData getGame(int gameID) throws DataAccessException;
+    void updateGame(int gameID, GameData updatedGame) throws DataAccessException;
 }
--- server/src/main/java/dataAccess/GameDAOMemory.java
+++ server/src/main/java/dataAccess/GameDAOMemory.java
@@ -3,18 +3,49 @@ package dataAccess;
 import chess.ChessGame;
 import model.GameData;
 
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.List;
 
 public class GameDAOMemory implements GameDAO {
-    private HashSet<GameData> games;
+    private final HashMap<Integer, GameData> games;
+    private int nextID;
 
     public GameDAOMemory() {
-        games = new HashSet<>();
+        games = new HashMap<>();
+        nextID = 1;
     }
 
     @Override
     public void clearGames() {
         games.clear();
+    }
+
+    @Override
+    public void createGame(String gameName) {
+        int id = nextID++;
+        GameData game = new GameData(id, null, null, gameName, new ChessGame());
+        games.put(id, game);
+    }
+
+    @Override
+    public List<GameData> listGames() {
+        return new ArrayList<>(games.values());
+    }
+
+    @Override
+    public GameData getGame(int gameID) throws DataAccessException {
+        if (!games.containsKey(gameID)) {
+            throw new DataAccessException("Game not found. Invalid gameID.");
+        }
+        return games.get(gameID);
+    }
+
+    @Override
+    public void updateGame(int gameID, GameData updatedGame) throws DataAccessException {
+        if (!games.containsKey(gameID)) {
+            throw new DataAccessException("Game not found. Invalid gameID.");
+        }
+        games.put(gameID, updatedGame);
     }
 }
